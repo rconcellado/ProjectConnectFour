91b9fc00dc22539f1b6c24d31478d30c994f3289
rconcellado <125625237+rconcellado@users.noreply.github.com>
2023-06-10 12:57:43 -0600

    Added the Board Layout in the Controller Class
    This board will initially display the 6 rows and 7 column boards that will play the players.

--- ProjectConnectFour/Program.cs
+++ ProjectConnectFour/Program.cs
@@ -1,10 +1,59 @@
 ﻿namespace ProjectConnectFour
 {
-    internal class Program
+    /* This will be the Model Class of the program
+     */
+    public class clsModel
     {
-        static void Main(string[] args)
+        private char[,] board;
+        public const int cntsRows = 6;
+        public const int cntsCol = 7;
+
+        public clsModel()
         {
-            Console.WriteLine("Hello, World!");
+            board = new char[cntsRows, cntsCol];
+           
+        }
+        /*This public void method will initialize the board apperance in
+         the initial run of the program
+        */
+        public void BoardLayOut()
+        {
+            Console.WriteLine("Connect 4 Game Development Project:");
+            //Loop though the Rows which have 6 Rows
+            for (int irow = 0; irow < cntsRows; irow++)
+            {
+                Console.Write("|");
+                //Loop through the columns which have 7 columns
+                for (int icol = 0; icol < cntsCol; icol++)
+                {   //Print the has - # for every board - row,column in the board
+                    Console.Write("  " + "#" + board[irow, icol] + " ");
+                }
+                Console.WriteLine(" |");
+            }
+            Console.WriteLine("   1   2   3   4   5   6   7 ");
+        }
+    }
+    /*This will be the controller class of the program
+     */
+    public class clsController
+    {
+        private clsModel model;
+        public clsController() { model = new clsModel(); }
+
+        public void PlayGame()
+        {
+            model.BoardLayOut();
+        }
+    }
+    /*Main class of the program that
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            clsController Controller = new clsController();
+            Controller.PlayGame();
+
         }
     }
 }
