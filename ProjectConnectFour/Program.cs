62e53864d6738c4452e593428bae5af9b6baa5dc
Reynaldo Concellado <125625237+rconcellado@users.noreply.github.com>
2023-06-23 13:03:34 -0600

    Added Player Abstract Class and Human Player Derived Class
    - Add Player abstract Class
    - Added Human Player Derived class to hold 2 players.
    - Remove unwanted class

--- ProjectConnectFour/Program.cs
+++ ProjectConnectFour/Program.cs
@@ -4,6 +4,29 @@ using System.Runtime.CompilerServices;
 
 namespace Connect4
 {
+    public abstract class Player
+    {
+        public  string Name { get; set; }
+        public char Id { get; set; }
+
+        public Player(string name, char id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+
+    public class HumanPlayers : Player
+    {
+        public string Name;
+        public char Id;
+
+        public HumanPlayers(string  playerName, char id) : base(playerName, id)
+        {
+
+        }
+    }
+
     public struct HumanPlayer
     {
         public string PlayerName;
@@ -42,11 +65,11 @@ namespace Connect4
             }
         }
 
-        public int PlayerDrop(HumanPlayer activePlayer)
+        public int PlayerDrop(Player activePlayer)
         {
             int choice;
 
-            Console.WriteLine(activePlayer.PlayerName + "'s Turn ");
+            Console.WriteLine(activePlayer.Name + "'s Turn ");
             do
             {
                 Console.WriteLine("Please enter a number between 1 and 7: ");
@@ -62,7 +85,7 @@ namespace Connect4
             return choice;
         }
 
-        public void CheckBellow(HumanPlayer activePlayer, int numselect)
+        public void CheckBellow(Player activePlayer, int numselect)
         {
             int length = 6;
             int turn = 0;
@@ -71,7 +94,7 @@ namespace Connect4
             {
                 if (board[length, numselect] != 'X' && board[length, numselect] != 'O')
                 {
-                    board[length, numselect] = activePlayer.PlayerID;
+                    board[length, numselect] = activePlayer.Id;
                     turn = 1;
                 }
                 else
@@ -81,9 +104,9 @@ namespace Connect4
             } while (turn != 1);
         }
 
-        public int CheckFour(HumanPlayer activePlayer)
+        public int CheckFour(Player activePlayer)
         {
-            char XO = activePlayer.PlayerID;
+            char XO = activePlayer.Id;
             int _win = 0;
 
             for (int i = 8; i >= 1; i--)
@@ -149,134 +172,6 @@ namespace Connect4
             return _full;
         }
 
-        public void Player_win(HumanPlayer activePlayer)
-        {
-            Console.WriteLine(activePlayer.PlayerName + " Connected Four! You _win!");
-        }
-
-
-    }
-
-    public class clsController
-    {
-        clsModel model;
-        private HumanPlayer _playerOne;
-        private HumanPlayer _playerTwo;
-        private char[,] board;
-        private int _numsel;
-        private int _win;
-        private int _full;
-        private int _another;
-
-        Game game = new Game();
-
-
-        public clsController()
-        {
-            //board = new char[9, 10];
-            _numsel = 0;
-            _win = 0;
-            _full = 0;
-            _another = 0;
-            model = new clsModel();
-        }
-
-        public void StartGame()
-        {
-            Console.WriteLine("Let's Play Connect Four");
-            Console.WriteLine("First Player, Please enter your name: ");
-            _playerOne.PlayerName = Console.ReadLine();
-            _playerOne.PlayerID = 'X';
-
-            Console.WriteLine("Second Player, Please enter your name: ");
-            _playerTwo.PlayerName = Console.ReadLine();
-            _playerTwo.PlayerID = 'O';
-
-            model.DisplayBoard();
-
-            do
-            {
-                _numsel = model.PlayerDrop(_playerOne);
-                model.CheckBellow(_playerOne, _numsel);
-                model.DisplayBoard();
-                _win = model.CheckFour(_playerOne);
-
-                if (_win == 1)
-                {
-                    model.Player_win(_playerOne);
-                    _another = game.Restart();
-                    if (_another == 2)
-                    {
-                        break;
-                    }
-                }
-                _numsel = model.PlayerDrop(_playerTwo);
-                model.CheckBellow(_playerTwo, _numsel);
-                model.DisplayBoard();
-                _win = model.CheckFour(_playerTwo);
-                if (_win == 1)
-                {
-                    model.Player_win(_playerTwo);
-                    _another = game.Restart();
-                    if (_another == 2)
-                    {
-                        break;
-                    }
-                }
-
-                _full = model._fullBoard();
-
-                if (_full == 7)
-                {
-                    Console.WriteLine("The board is _full. It is a draw!");
-                    _another = game.Restart();
-                }
-
-            } while (_another != 2);
-        }
-    }
-
-    public class GameBoard
-    {
-        private char[,] board;
-        private int rows;
-        private int columns;
-
-        public GameBoard()
-        {
-            rows = 6;
-            columns = 7;
-            board = new char[rows, columns];
-        }
-
-        public void DisplayBoard()
-        {
-            for (int i = 0; i < rows; i++)
-            {
-                Console.Write("|");
-                for (int j = 0; j < columns; j++)
-                {
-                    if (board[i, j] != 'X' && board[i, j] != 'O')
-                    {
-                        board[i, j] = '*';
-                    }
-                    Console.Write(board[i, j]);
-                }
-
-                Console.Write("| \n");
-            }
-        }
-    }
-
-    public class Game
-    {
-        private char[,] board;
-
-        public Game()
-        {
-            board = new char[6, 7];
-        }
-
         public int Restart()
         {
             int restart;
@@ -301,14 +196,102 @@ namespace Connect4
 
             return restart;
         }
+
+        public void Player_win(Player activePlayer)
+        {
+            Console.WriteLine(activePlayer.Name + " Connected Four! You _win!");
+        }
+
+
+    }
+
+    public class clsController
+    {
+        clsModel model;
+        private char[,] board;
+        private int _numsel;
+        private int _win;
+        private int _full;
+        private int _another;
+
+        private Player _player1;
+        private Player _player2;
+
+        public clsController()
+        {
+            _numsel = 0;
+            _win = 0;
+            _full = 0;
+            _another = 0;
+            model = new clsModel();
+        }
+
+        public void SetPlayers(Player player1 , Player player2)
+        {
+            this._player1 = player1;
+            this._player2 = player2;
+        }
+
+        public void StartGame()
+        {
+            model.DisplayBoard();
+
+            do
+            {
+                _numsel = model.PlayerDrop(_player1);
+                model.CheckBellow(_player1, _numsel);
+                model.DisplayBoard();
+                _win = model.CheckFour(_player1);
+
+                if (_win == 1)
+                {
+                    model.Player_win(_player1);
+                    _another = model.Restart();
+                    if (_another == 2)
+                    {
+                        break;
+                    }
+                }
+                _numsel = model.PlayerDrop(_player2);
+                model.CheckBellow(_player2, _numsel);
+                model.DisplayBoard();
+                _win = model.CheckFour(_player2);
+                if (_win == 1)
+                {
+                    model.Player_win(_player2);
+                    _another = model.Restart();
+                    if (_another == 2)
+                    {
+                        break;
+                    }
+                }
+
+                _full = model._fullBoard();
+
+                if (_full == 7)
+                {
+                    Console.WriteLine("The board is _full. It is a draw!");
+                    _another = model.Restart();
+                }
+
+            } while (_another != 2);
+        }
     }
 
     public class Program
     {
         static void Main(string[] args)
         {
+            HumanPlayers Player1 = new HumanPlayers("Gabriel", 'X');
+            HumanPlayers Player2 = new HumanPlayers("Rey", 'O');
+
             clsController controller = new clsController();
+
+            controller.SetPlayers(Player1, Player2);
+
             controller.StartGame();
+
+
         }
     }
 }
